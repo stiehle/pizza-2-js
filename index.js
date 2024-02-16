218b538630defff925cc5a1edaf3836f1e0d9849
stiehle <gstiehle@yahoo.de>
2024-02-16 22:41:05 +0100

    first-commit

---
+++ index.js
@@ -0,0 +1,8 @@
+console.log("Hallo");
+
+const element = document.getElementById("menue");
+
+function showMenue() {
+  element.classList.toggle("menue-off");
+  element.classList.toggle("menue-on");
+}
