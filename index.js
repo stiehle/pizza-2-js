cd59699c90e31583ed31577ceb55617902f1647c
stiehle <gstiehle@yahoo.de>
2024-02-25 17:59:46 +0100

    second commit

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-console.log("Hallo");
-
-const element = document.getElementById("menue");
+// const element = document.getElementById("menue");
+const element2 = document.getElementById("menu-wrapper");
 
 function showMenue() {
-  element.classList.toggle("menue-off");
-  element.classList.toggle("menue-on");
+  // element.classList.toggle("menue-off");
+  // element.classList.toggle("menue-on");
+
+  element2.classList.toggle("menu-wrapper-off");
+  element2.classList.toggle("menu-wrapper-on");
 }
